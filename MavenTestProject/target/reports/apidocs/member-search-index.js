7a0c07616067d1ef6a4aa98343c8edfbe659ffec
Makarfran <makarfranx1@gmail.com>
2025-07-03 19:51:21 +0200

    fran: proyecto maven inicial

---
+++ MavenTestProject/target/reports/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"umu.tds.apps","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"umu.tds.apps","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ MavenTestProject/target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"umu.tds.apps","l":"App"}];updateSearchResults();
